e3a0284ddc873985c9a0c626814d081c7fa84417
Marcio Endo <marcio.endo@objectos.com.br>
2014-01-14 16:00:21 -0200

    Itaú: corrige nosso número

--- src/test/java/br/com/objectos/way/boleto/TesteDeNossoNumero.java
+++ src/test/java/br/com/objectos/way/boleto/TesteDeNossoNumero.java
@@ -11,8 +11,6 @@ import static org.hamcrest.Matchers.equalTo;
 
 import org.testng.annotations.Test;
 
-import br.com.objectos.way.boleto.NossoNumero;
-
 /**
  * @author marcio.endo@objectos.com.br (Marcio Endo)
  */
@@ -22,31 +20,37 @@ public class TesteDeNossoNumero {
   public void numero() {
     String numero = "1234";
 
-    NossoNumero res = new NossoNumero(numero);
+    NossoNumero res = NossoNumero.parseString(numero);
 
     assertThat(res.longValue(), equalTo(123l));
     assertThat(res.getDigito(), equalTo('4'));
   }
 
-  @Test(expectedExceptions = IllegalArgumentException.class)
-  public void nao_numero() {
-    String numero = "abc";
-
-    new NossoNumero(numero);
-  }
-
-  @Test(expectedExceptions = IllegalArgumentException.class)
   public void nao_numero_hifen() {
     String numero = "123-4";
 
-    new NossoNumero(numero);
+    NossoNumero res = NossoNumero.parseString(numero);
+
+    assertThat(res.longValue(), equalTo(123l));
+    assertThat(res.getDigito(), equalTo('4'));
   }
 
-  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void nao_numero() {
+    String numero = "abc";
+
+    NossoNumero res = NossoNumero.parseString(numero);
+
+    assertThat(res.longValue(), equalTo(0l));
+    assertThat(res.getDigito(), equalTo('\0'));
+  }
+
   public void nao_numero_vazio() {
     String numero = "";
 
-    new NossoNumero(numero);
+    NossoNumero res = NossoNumero.parseString(numero);
+
+    assertThat(res.longValue(), equalTo(0l));
+    assertThat(res.getDigito(), equalTo('\0'));
   }
 
 }
